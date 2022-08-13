e6ce1cea73f64e60b30f367f96b2937c542ca1a4
van-matos <vanmatos@poli.ufrj.br>
2022-08-13 17:25:38 -0300

    fix: edit validation

--- src/middlewares/postMiddleware.js
+++ src/middlewares/postMiddleware.js
@@ -1,14 +1,10 @@
 import { getPost } from "../repositories/postsRepository.js";
 
 export async function editValidation (req, res, next) {
-    console.log(req.params.id);
-
     const userId = res.locals.dados.id;
     const { id: postId } = req.params;
 
     const post = await getPost(postId);
-
-    console.log(userId);
 
     if (!post) {
         return res.status(404).send("This post does not exist.")
